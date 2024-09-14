8f64892e3cb576f849ccb2a67e4590ec9322a83f
RobiulArman <robiularman5@gmail.com>
2024-09-15 01:00:50 +0600

    added some code

--- dom.js
+++ dom.js
@@ -1,1 +1,25 @@
 // console.log("hey")
+
+const sections =  document.querySelectorAll('.q-title');
+for(const section of sections)
+{
+    section.style.border = "2px solid red";
+    section.style.backgroundColor = "red";
+    section.style.textAlign = "center";
+}
+
+const mainContainer = document.getElementById("container");
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = 
+`
+<h1>This is my dress </h1>
+<ul>
+<li>shirt</li>
+<li>shirt</li>
+<li>shirt</li>
+<li>shirt</li>
+</ul>
+`
+mainContainer.appendChild(sectionDress);
+
+
