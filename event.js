0c82d563ee3e2f1a89d9e748cf83570615d3b3e1
RobiulArman <robiularman5@gmail.com>
2024-09-16 01:07:36 +0600

    event

---
+++ event.js
@@ -0,0 +1,14 @@
+
+//system 1
+function makepurple()
+        {
+            document.body.style.backgroundColor='purple';
+        }
+
+//system 2
+
+const blueButton = document.getElementById("blue")
+blueButton.onclick = function makeBlue()
+{
+    document.body.style.backgroundColor = 'blue';
+}
